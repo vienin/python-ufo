1f1c8ef7b4bf74461997e48f7cf3b64ad25ff85f
Sylvain Baubeau <sylvain.baubeau@agorabox.org>
2011-03-15 17:57:28 +0100

    Removed useless parameters for a client

--- ufo/config.py
+++ ufo/config.py
@@ -26,9 +26,7 @@ env._bootstrap()
 env._finalize_core()
 
 try:
-
     server = ""
-
 
     ufo_in_server = env.host in env.xmlrpc_uri
 
@@ -42,14 +40,6 @@ try:
     ##
     realm = env.realm.encode()
 
-    # Only member of this group can call the Storage component procedures
-    storage_admins_group = 'ufoadmins'
-
-    # the root principal on the nfs server. See also the idmapd.conf
-    # your server
-    nfsadmin_principal = 'nfsadmin@%s' % env.realm.encode()
-
-
     ######################################################
     # Storage
     ######################################################
@@ -57,20 +47,12 @@ try:
     # this variable is used by the Sync component
     storage_host = 'https://%s/xmlrpc' % server
 
-    # directory where shares can be finded 
-    fs_mount_point = u"/mnt/nfs/shares/"
-
-    # the admin nfs keytab
-    nfs_admin_keytab = '/etc/httpd/conf/nfs.keytab'
-
     ######################################################
     # Sync
     ######################################################
     # address of the host providing the Storage Component
     # this variable is used by the Account component
     sync_host = 'https://%s/xmlrpc' % server
-    sync_public_user = 'public'
-    sync_public_user_keytab = '/etc/httpd/conf/public.keytab'
 
     ######################################################
     # Account
@@ -78,25 +60,6 @@ try:
     # address of the host providing the Account Component
     # this variable is used by the other components
     account_host = 'https://%s/xmlrpc' % server
-
-    ######################################################
-    # CouchDB
-    ######################################################
-    couchdb_host = "http://%s:5984" % server
-
-    ######################################################
-    # Sync Spawner
-    ######################################################
-    sync_spawner_port = 5000
-    sync_spawner_authkey = "jk28jsdf_39!39FksFd"
-
-    ######################################################
-    # Quota daemon
-    ######################################################
-    quota_daemon_principal = "HTTP/%s@%s" % (server, realm)
-    quota_daemon_port = 20000
-    quota_daemon_authkey = "kjsdn,,2378(,sdfljs!"
-    nfs_export = "/"
 
 except AttributeError, e:
     # seems that config file doesn't contains xmlrpc_uri
